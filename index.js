0b99e7d744038e1230b64bc687d831079a031440
sjosh5416 <sjosh5416@gmail.com>
2022-06-19 14:52:18 +0800

    commit

--- index.js
+++ index.js
@@ -3,7 +3,28 @@ createApp({
   data() {
     return {
       header: "todolist",
+      newTodo: "",
+      todo: [
+        { status: false, text: "看電視" },
+        { status: false, text: "打電動" },
+      ],
     };
   },
-  methods: {},
+  methods: {
+    addTodo() {
+      if (this.newTodo == "") {
+        return;
+      }
+      const temp = { status: false, text: this.newTodo };
+      this.todo.push(temp);
+      console.table(this.todo);
+    },
+    deleteTodo(i) {
+      this.todo.splice(i, 1);
+    },
+    finishTodo(i) {
+      this.todo[i].status = true;
+      console.table(this.todo);
+    },
+  },
 }).mount("#app");
